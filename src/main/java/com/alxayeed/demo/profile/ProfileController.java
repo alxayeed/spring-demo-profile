0ec06254b4a2958040ddc8f7222b5218c3a1e9aa
alxayeed <alxayeed@gmail.com>
2021-09-23 11:59:53 +0600

    emty service created

--- src/main/java/com/alxayeed/demo/profile/ProfileController.java
+++ src/main/java/com/alxayeed/demo/profile/ProfileController.java
@@ -8,30 +8,40 @@ import java.util.List;
 @RequestMapping(path="api/v1/profile")
 public class ProfileController {
 
+    private final ProfileService profileService;
+
+    public ProfileController(ProfileService profileService) {
+        this.profileService = profileService;
+    }
+
     @GetMapping
     public List<Profile> getProfiles(){
         //return list of students
-
+        return profileService.getProfiles();
     }
 
     @GetMapping
     public Profile getProfile(){
         // returns single user profile object
+        return profileService.getProfile();
     }
 
     @PostMapping
     public Profile addProfile(){
         // adds a profile object
+        return profileService.addProfile();
     }
 
     @PutMapping
     public Profile updateProfile (){
         // update profile object
+        return profileService.updateProfile();
     }
 
     @DeleteMapping
     public void deleteProfile(){
         // deletes a profile object
+        profileService.deleteProfile();
     }
 
 
---
+++ src/main/java/com/alxayeed/demo/profile/ProfileService.java
@@ -0,0 +1,20 @@
+package com.alxayeed.demo.profile;
+
+import java.util.List;
+
+public class ProfileService {
+    public List<Profile> getProfiles() {
+    }
+
+    public Profile getProfile() {
+    }
+
+    public void deleteProfile() {
+    }
+
+    public Profile updateProfile() {
+    }
+
+    public Profile addProfile() {
+    }
+}
